193f70b742033923dc7af9f46811f557318df844
shota <shotalapachi13@gmail.com>
2023-06-11 22:31:36 +0400

    1

---
+++ Assignment5/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int SumOfDigitsOnIndex(int[] array, int index)
+{
+    int sum = 0;
+    int x = array[index];
+    while (x > 0)
+    {
+        sum += x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+
+int index = 0;
+int arrsize;
+
+Console.WriteLine("Input your array size");
+arrsize = int.Parse(Console.ReadLine());
+
+int[] userarr = new int[arrsize];
+
+for (int i = 0;i < arrsize;i++)
+{
+    Console.WriteLine("intput "+i+"index value");
+    userarr[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Choose index from 0-" + (arrsize - 1));
+index = int.Parse(Console.ReadLine());
+    
+Console.WriteLine(SumOfDigitsOnIndex(userarr, index));
---
+++ Assignment5/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+char[] CreateArray()
+{
+    Console.WriteLine("Input your array size");
+    int ArraySize = int.Parse(Console.ReadLine());
+    char[] Array = new char[ArraySize];
+    Console.WriteLine("Input " + ArraySize + " elements");
+    for (int i = 0; i < ArraySize; i++)
+        Array[i] = char.Parse(Console.ReadLine());
+    return Array;
+}
+
+int CharCount(char[] Array, char Char)
+{
+    int Count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] == Char)
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+void PrintResults()
+{
+    Console.WriteLine("Input any char");
+    char Char = char.Parse(Console.ReadLine());
+    Console.WriteLine(CharCount(CreateArray(), Char));
+}
+
+PrintResults();
